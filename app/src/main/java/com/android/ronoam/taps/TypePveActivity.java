ae5db46d71e0151fd05fae9805fa3b6ae0b420d6
Noam <noamtayri1990@gmail.com>
2018-07-21 17:22:14 +0300

    type_pve design

--- app/src/main/java/com/android/ronoam/taps/TypePveActivity.java
+++ app/src/main/java/com/android/ronoam/taps/TypePveActivity.java
@@ -2,11 +2,15 @@ package com.android.ronoam.taps;
 
 import android.app.Activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import com.android.ronoam.taps.Keyboard.KeyboardWrapper;
 import com.android.ronoam.taps.Utils.MyToast;
+
+import org.w3c.dom.Text;
 
 
 public class TypePveActivity extends Activity {
@@ -20,6 +24,13 @@ public class TypePveActivity extends Activity {
         setContentView(R.layout.activity_type_pve);
 
         mKeyboardWrapper = new KeyboardWrapper(this, R.id.keyboard_view, R.xml.heb_qwerty);
+
+        Typeface AssistantBoldFont = Typeface.createFromAsset(getAssets(),  "fonts/Assistant-Bold.ttf");
+        Typeface AssistantExtraBoldFont = Typeface.createFromAsset(getAssets(),  "fonts/Assistant-ExtraBold.ttf");
+        TextView timer = findViewById(R.id.keyboard_game_timer);
+        TextView counter = findViewById(R.id.keyboard_game_counter);
+        timer.setTypeface(AssistantExtraBoldFont);
+        counter.setTypeface(AssistantBoldFont);
 
         startGame();
     }
